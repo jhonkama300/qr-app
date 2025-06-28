44ff24249491e6ba6a570fb73ff65c47684fd4a8
jhonkama300 <jhonkama300@gmail.com>
2025-06-28 06:29:48 -0500

    subiendo nuevos cambios

--- lib/firebase.ts
+++ lib/firebase.ts
@@ -4,12 +4,12 @@ import { getFirestore } from "firebase/firestore"
 
 // Configuración de Firebase - Reemplaza con tus credenciales
 const firebaseConfig = {
-  apiKey: process.env.NEXT_PUBLIC_FIREBASE_API_KEY,
-  authDomain: process.env.NEXT_PUBLIC_FIREBASE_AUTH_DOMAIN,
-  projectId: process.env.NEXT_PUBLIC_FIREBASE_PROJECT_ID,
-  storageBucket: process.env.NEXT_PUBLIC_FIREBASE_STORAGE_BUCKET,
-  messagingSenderId: process.env.NEXT_PUBLIC_FIREBASE_MESSAGING_SENDER_ID,
-  appId: process.env.NEXT_PUBLIC_FIREBASE_APP_ID,
+  apiKey: "AIzaSyCs6WeOtOh6cxjyBYTXmO4zaaNNnAzJ8-w",
+  authDomain: "qr-access-a29ad.firebaseapp.com",
+  projectId: "qr-access-a29ad",
+  storageBucket: "qr-access-a29ad.firebasestorage.app",
+  messagingSenderId: "172471519148",
+  appId: "1:172471519148:web:2404efb1cdf871bec0dc09",
 }
 
 // Inicializar Firebase
